0665af62fafa2b962458698f053b6818aaad36b9
Alex <filippov.pda@gmail.com>
2020-02-05 21:30:54 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+	
+	public static void main(String... args) {
+//		System.out.println(String.class.toString());
+		String[] array = {"1", "3", "4"};
+		String[] newArray = Arrays.copyOf(array, 15);
+		Arrays.fill(newArray, array.length, newArray.length, "#");
+		System.out.println(Arrays.toString(array));
+		System.out.println(Arrays.toString(newArray).replaceAll("[\\[, \\]]", ""));
+	}
+
+}
